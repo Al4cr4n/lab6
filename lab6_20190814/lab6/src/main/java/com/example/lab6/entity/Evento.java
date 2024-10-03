1caf81048619e5faca9ae94c98c125e6c9ff6e68
Al4cr4n <148933724+Al4cr4n@users.noreply.github.com>
2024-10-02 21:04:53 -0500

    Segundo comentario

--- lab6_20190814/lab6/src/main/java/com/example/lab6/entity/Evento.java
+++ lab6_20190814/lab6/src/main/java/com/example/lab6/entity/Evento.java
@@ -8,7 +8,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
-import jakarta.validation.constraints.*;
+
 
 import java.time.LocalDate;
 
@@ -22,18 +22,10 @@ public class Evento {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "eventoId")
     private Integer id;
-
-    @NotBlank(message = "El nombre del evento es obligatorio")
-    @Size(min = 3, max = 100, message = "El nombre debe tener entre 3 y 100 caracteres")
     private String nombre;
 
-    @NotNull(message = "La fecha del evento es obligatoria")
-    @Future(message = "La fecha del evento debe ser futura")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate fecha;
-
-    @NotNull(message = "El número de asistentes esperados es obligatorio")
-    @Min(value = 1, message = "El número de asistentes esperados debe ser positivo")
     private Integer asistentesEsperados;
 
     // Otros atributos o relaciones
